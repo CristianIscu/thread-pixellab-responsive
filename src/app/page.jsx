4a48790362ae178bbd84dd0f6cd1adcb3df7fed9
Cristian Iscu <cristianiscu@gmail.com>
2023-08-17 21:59:23 +0300

    17.08.23

---
+++ src/app/page.jsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <>
+    <header class="header"><i class="fa-solid fa-arrow-right"></i>
+    </header>
+
+    <main class="content mt-4">
+      <picture>
+      <img src="/images/logo.jpg" alt="asdaad"></img>
+      </picture>
+      </main>
+
+    <footer class="footer">footer</footer>
+    </>
+  )
+}
