08427a0c2d0dafb863642031909e9856ee74a1bb
Cristian Iscu <cristianiscu@gmail.com>
2023-08-27 18:01:38 +0300

    Tema 24.08.2023

--- src/app/components/common/Footer.jsx
+++ src/app/components/common/Footer.jsx
@@ -7,112 +7,118 @@ export default function Footer() {
         <nav className="footer-nav">
         <ul>
           <li>
-            <a href="/" title="">Information</a>
+            <a href="/" title="Track my order">Track my order</a>
           </li>
 
           <li>
-            <a href="/" title="">Information</a>
+            <a href="/" title="Shipping">Shipping</a>
           </li>
 
           <li>
-            <a href="/" title="">Information</a>
+            <a href="/" title="Free returns*">Free returns*</a>
           </li>
 
           <li>
-            <a href="/" title="">Information</a>
-          </li>
-
-          <li>
-            <a href="/" title="">Information</a>
+            <a href="/" title="My account">My account</a>
           </li>
         </ul>
 
         <ul>
           <li>
-            <a href="/" title="">Information</a>
+            <a href="/" title="Our story">Our story</a>
           </li>
 
           <li>
-            <a href="/" title="">Information</a>
+            <a href="/" title="Art & culture">Art & culture</a>
           </li>
 
           <li>
-            <a href="/" title="">Information</a>
-          </li>
-        </ul>
-
-        <ul>
-          <li>
-            <a href="/" title="">Information</a>
+            <a href="/" title="Our private labels">Our private labels</a>
           </li>
 
           <li>
-            <a href="/" title="">Information</a>
-          </li>
-
-          <li>
-            <a href="/" title="">Information</a>
-          </li>
-
-          <li>
-            <a href="/" title="">Information</a>
+            <a href="/" title="Stores">Stores</a>
           </li>
         </ul>
 
         <ul>
           <li>
-            <a href="/" title="">Information</a>
+            <a href="/" title="Services">Services</a>
           </li>
 
           <li>
-            <a href="/" title="">Information</a>
+            <a href="/" title="Ship to store">Ship to store</a>
           </li>
 
           <li>
-            <a href="/" title="">Information</a>
+            <a href="/" title="Gift card">Gift card</a>
           </li>
 
           <li>
-            <a href="/" title="">Information</a>
+            <a href="/" title="Online only">Online only</a>
           </li>
 
           <li>
-            <a href="/" title="">Information</a>
+            <a href="/" title="Tips & tricks">Tips & tricks</a>
           </li>
 
           <li>
-            <a href="/" title="">Information</a>
+            <a href="/" title="Home">Home</a>
+          </li>
+        </ul>
+
+        <ul>
+          <li>
+            <a href="/" title="Loyalty programs">Loyalty programs</a>
+          </li>
+
+          <li>
+            <a href="/" title="Sign up to the simons">Sign up to the simons</a>
+          </li>
+
+          <li>
+            <a href="/" title="My account">My account</a>
+          </li>
+
+          <li>
+            <a href="/" title="Conditions">Conditions</a>
+          </li>
+
+          <li>
+            <a href="/" title="Faq">Faq</a>
           </li>
         </ul>
       </nav>
 
       <section className="footer-contact">
-        <h1><span>Thread Affair</span>Milano</h1>
+        <h1><span>Thread Affair</span> Milano</h1>
         <p>
-          Largo Augusto 3, Milano, 20122, Milano, IT Tel: 0039 02 760 003 66 office@thread-affair.com
+          Largo Augusto 3, Milano, 20122, Milano, IT <br />
+         <a href="tel:+0039 02 760 003 66">Tel: 0039 02 760 003 66</a><br />
+          <a href="mailto:office@thread-affair.com">office@thread-affair.com</a>
         </p>
       </section>
 
       <section className="footer-connect">
         <ul>
           <li>
-            <a href="" title="">
-              <i className="fa-solid"></i>
+            <a href="tel:+0039 02 760 003 66" title="Support">
+              <i className="fa-solid fa-phone"></i> <br />
               Support
             </a>
           </li>
 
           <li>
-            <a href="" title="">
-              <i className="fa-solid"></i>
-              Support
+            <a href="/" title="Chat">
+              <i className="fa-solid fa-comments"></i><br />
+              Chat
             </a>
           </li>
 
           <li>
-            <a href="" title="">
-              <i className="fa-solid"></i>
-              Support
+            <a href="/" title="Email">
+              <i className="fa-solid fa-envelope"></i><br />
+              Email
             </a>
           </li>
         </ul>
@@ -121,7 +127,10 @@ export default function Footer() {
     </div>
 
     <div className="footer-secondary">
-      Homework
+      <p>2020 THREAD AFFAIR. All Rights Reserved.</p>
+      <p>Designed by
+          <a href="http://pixellab.ro" target="_blank" rel="noopener noreferrer" title="Pixellab.ro"><img src="/images/pixellab_logo.jpg" alt="Pixellab" srcset="" /></a>
+      </p>
     </div>
     </>
   );
